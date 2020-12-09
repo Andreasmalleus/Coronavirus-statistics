7d005c035a4e07b7b36566b2a20542951202eceb
Andreasmalleus <andreasmalleus@gmail.com>
2020-12-09 18:05:33 +0200

    no news message

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 const News = ({data, country}) => {
-    console.log(data);
     return (
         <div className="news">
             <div className="news-title">Top news for {country}</div>
             {
-                data != []
+                data.length != 0
                 ?
                 data.map((article, id) => (
                     <a href={article.url} key={id}>
@@ -21,7 +20,7 @@ const News = ({data, country}) => {
                     </a>
                 ))
                 :
-                <div>No news found</div>
+                <div className="no-news">No news found....</div>
             }
         </div>
     )
