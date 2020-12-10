f5a7a65b4299c57da63c526b3e3f65fc4390960c
Andreasmalleus <andreasmalleus@gmail.com>
2020-12-10 18:50:24 +0200

    author added

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const News = ({data, country}) => {
+
+    console.log(data);
+
     return (
         <div className="news">
             <div className="news-title">Top news for {country}</div>
@@ -11,7 +14,7 @@ const News = ({data, country}) => {
                     <a href={article.url} key={id}>
                         <div className="article">
                             <div className="content">
-                                <div className="author">author</div>
+                                <div className="author">{article.source.name}</div>
                                 <div className="title">{article.title}</div>
                                 <div className="date">{article.publishedAt}</div>
                             </div>
